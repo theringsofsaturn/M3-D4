bf384cf20e255a49c1d2a1e6d6720f9feb2259c5
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-20 16:39:48 +0200

    commits

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function renderBooks() {
       });
     });
 }
+
+function addToCard() {
+  const cardList = document.getElementById("card-list");
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((response) => response.json())
+    .then((books) => {
+      books.forEach((element) => {
+        cardList.innerHTML += `<div class="card">
+                    <img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">
+                            Some quick example text to build on the card title
+                            and make up the bulk of the card's content.
+                        </p>
+                        <a href="#!" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>`;
+      });
+    });
+}
