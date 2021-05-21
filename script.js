af9e9c34cc61d2d76a6f3964af5e5dc9181a3392
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-05-21 23:06:10 +0200

    commit

--- script.js
+++ script.js
@@ -21,23 +21,24 @@ function renderBooks() {
     });
 }
 
-function addToCard() {
-  const cardList = document.getElementById("card-list");
-  fetch("https://striveschool-api.herokuapp.com/books")
+window.onload = function () {
+  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
     .then((response) => response.json())
-    .then((books) => {
-      books.forEach((element) => {
-        cardList.innerHTML += `<div class="card">
-                    <img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h4 class="card-title">Card title</h4>
-                        <p class="card-text">
-                            Some quick example text to build on the card title
-                            and make up the bulk of the card's content.
-                        </p>
-                        <a href="#!" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>`;
-      });
+    .then((musica) => {
+      console.log(musica);
+      const parent = document.getElementById("results");
+      parent.innerHTML = musica
+        .map(
+          (musica) =>
+            `<div class="card col-4" >
+          <img src="${musica.data.cover}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${musica.data.title}</h5>
+            <p class="card-text">Price ${musica.data.artist.name}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>`
+        )
+        .join("");
     });
-}
+};
